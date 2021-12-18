4d0abadcac4e9ac2e02984659a9b9cf82ad15dcc
yannong <yannonghuang@icloud.com>
2021-12-18 14:21:41 -0800

    my response under my shinshin

---
+++ backend/app/migrations/20211218202146-add_users_responses_association.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    return Promise.all([
+      queryInterface.addColumn(
+        'responses', // name of Source model
+        'userId', // name of the key we're adding
+        {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'users', // name of Target model
+            key: 'id', // key in Target model that we're referencing
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+        }
+      ),
+
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all([
+      queryInterface.removeColumn('responses', 'userId'),
+    ]);
+  }
+};
