3279f072e015b97fcd9ecde4eaedce296563f861
yannong <yannonghuang@icloud.com>
2021-12-25 16:16:29 -0800

    timeout 20 minutes on the client side

---
+++ react-app/src/services/timer.service.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { withRouter } from "react-router-dom";
+import { useIdleTimer } from 'react-idle-timer';
+//import { useHistory } from 'react-router'
+
+import AuthService from "./auth.service";
+
+const SESSION_IDEL_MINUTES = 20;
+
+const AutoLogoutTimer = (props: any) => {
+    const { ComposedClass, ...passThroughProps } = props;
+    //const history = useHistory();
+
+    const handleOnIdle = (event: any) => {
+        //console.log('user is idle', event)
+        //console.log('last active', getLastActiveTime())
+        AuthService.logout();
+        props.history.push('/login');
+    }
+
+    const {getLastActiveTime } = useIdleTimer({
+        timeout: 1000 * 60 * SESSION_IDEL_MINUTES,
+        onIdle: handleOnIdle,
+        debounce: 500,
+    })
+
+    return <ComposedClass  {...passThroughProps} />
+}
+
+export default withRouter(AutoLogoutTimer);
