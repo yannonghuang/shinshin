9bfeaf62d4cd2be30f41ed2993a1318c955bb092
yannong <yannonghuang@icloud.com>
2022-01-16 13:39:58 -0800

    different modes for user: contact only or normal

--- react-app/src/components/register.component.js
+++ react-app/src/components/register.component.js
@@ -520,7 +520,7 @@ export default class Register extends Component {
           {!this.state.successful && (
             <div class="row">
                 <div class="form-group col-md-4">
-                  <label htmlFor="contactOnly">仅联络方式(信息不作校验)</label>
+                  <label htmlFor="contactOnly"><h5>仅联络方式(信息不作校验)</h5></label>
                   <Input
                     readonly={!this.state.newuser?"":false}
                     type="checkbox"
