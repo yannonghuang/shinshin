bf92440407a538655a0acfdfc884c88a2e119c52
yannong <yannonghuang@icloud.com>
2022-02-22 23:18:33 -0800

    build/deploy/production

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  //origin: "http://localhost:8081"
 };
 
 app.use(cors(corsOptions));
