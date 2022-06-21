8f69c2daa3745e0b5d1148c8c457f646e9a0b606
yannong <yannonghuang@icloud.com>
2022-06-21 14:33:58 -0700

    form list formating

--- react-app/src/components/forms-list.component.js
+++ react-app/src/components/forms-list.component.js
@@ -345,7 +345,7 @@ const FormsList = (props) => {
           <input
             type="text"
             readonly=""
-            className="form-control col-sm-1 ml-2"
+            className="form-control col-sm-2 ml-2"
             placeholder="年份"
             value={searchStartAt}
             onChange={onChangeSearchInputStartAt}
