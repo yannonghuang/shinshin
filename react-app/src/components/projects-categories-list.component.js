cf5af76619de164e4cfc8b45fecd2e9874081e85
yannong <yannonghuang@icloud.com>
2022-08-10 15:32:02 -0700

    project categories

---
+++ react-app/src/components/projects-categories-list.component.js
@@ -0,0 +1,241 @@
+import React, { Component } from "react";
+import ProjectDataService from "../services/project.service";
+import SchoolDataService from "../services/school.service";
+import { Link } from "react-router-dom";
+import Pagination from "@material-ui/lab/Pagination";
+
+import { useState, useEffect, useMemo, useRef, useCallback } from "react";
+import { useTable, useSortBy, useFlexLayout } from "react-table";
+
+import YearPicker from 'react-single-year-picker';
+
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+
+import AuthService from "./../services/auth.service";
+
+const ProjectsByCategoriesList = (props) => {
+  const [projects, setProjects] = useState([]);
+
+  const [currentProject, setCurrentProject] = useState(null);
+
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
+  const [pCategoryId, setPCategoryId] = useState(props.match? props.match.params.pCategoryId : props.pCategoryId);
+
+
+  const projectsRef = useRef();
+  projectsRef.current = projects;
+
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
+  const [pageSize, setPageSize] = useState(30);
+
+  const [totalItems, setTotalItems] = useState(0);
+
+  const pageSizes = [20, 30, 50];
+
+  const [categories, setCategories] = useState(ProjectDataService.PROJECT_CATEGORIES);
+
+  const getRequestParams = () => {
+    let params = {};
+
+    if (pCategoryId) {
+      params["pCategoryId"] = pCategoryId;
+    }
+
+    return params;
+  };
+
+/**
+  const getCategories = () => {
+    ProjectDataService.getCategories()
+      .then(response => {
+        setCategories(response.data);
+        console.log(response);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  useEffect(getCategories, []);
+*/
+
+  const retrieveProjects = () => {
+    const params = getRequestParams();
+
+    ProjectDataService.getAllByCategories(params)
+      .then((response) => {
+        const { projects, totalPages, totalItems } = response.data;
+
+        setProjects(projects);
+        setCount(totalPages);
+        setTotalItems(totalItems);
+
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(retrieveProjects, [page, pageSize]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "项目年份",
+        accessor: "startAt",
+      },
+      {
+        Header: "学校项目数",
+        accessor: "count",
+        Cell: (props) => {
+          const rowIdx = props.row.id;
+          return (
+            <div>
+
+              <Link
+                to={"/projectsByCategoryByStartAt/" + projectsRef.current[rowIdx].pCategoryId +
+                    "/" + projectsRef.current[rowIdx].startAt}
+              >
+                {projectsRef.current[rowIdx].count}
+              </Link>
+
+            </div>
+          );
+        },
+      },
+    ],
+    []
+  );
+
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    state: {sortBy},
+  } = useTable({
+    columns,
+    data: projects,
+  });
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
+  const handlePageSizeChange = (event) => {
+    setPageSize(event.target.value);
+    setPage(1);
+  };
+
+
+
+  return (
+    <div className="list row">
+      <div className="col-sm-9">
+        <h4>
+          {'项目类型：' + categories[pCategoryId] }(项目总数：{totalItems})
+        </h4>
+      </div>
+
+      <div className="mt-3 col-sm-3">
+        {"每页显示行数: "}
+        <select onChange={handlePageSizeChange} value={pageSize}>
+          {pageSizes.map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+
+        <Pagination
+          className="my-3"
+          count={count}
+          page={page}
+          siblingCount={1}
+          boundaryCount={1}
+          variant="outlined"
+          shape="rounded"
+          onChange={handlePageChange}
+        />
+      </div>
+
+        <div class="w-100"></div>
+
+      <div className="col-sm-12 list">
+        <table
+          className="table table-striped table-bordered"
+          {...getTableProps()}
+        >
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  // Add the sorting props to control sorting. For this example
+                  // we can add them into the header props
+                  <th {...column.getHeaderProps()}>
+                    {column.render('Header')}
+                    {/* Add a sort direction indicator */}
+                    <span>
+                      {/*column.isSorted*/ (column.id === 'school.region' || column.id === 'school.code' ||
+                      column.id === 'school.name' || column.id === 'status'
+                      || column.id === 'name' )
+                      ? column.isSortedDesc
+                        ? ' 🔽'
+                        : ' 🔼'
+                      : ''}
+                    </span>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="mt-3 col-sm-">
+        {"每页显示行数: "}
+        <select onChange={handlePageSizeChange} value={pageSize}>
+          {pageSizes.map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+
+        <Pagination
+          className="my-3"
+          count={count}
+          page={page}
+          siblingCount={1}
+          boundaryCount={1}
+          variant="outlined"
+          shape="rounded"
+          onChange={handlePageChange}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default ProjectsByCategoriesList;
