436dee06368a2c565a078a7dfa3dbe9986c6f224
yannong <yannonghuang@icloud.com>
2022-10-15 09:54:59 -0700

    donor management

--- backend/app/controllers/batch.controller.js
+++ backend/app/controllers/batch.controller.js
@@ -101,6 +101,8 @@ const uploadDonations = async (req, res) => {
         }
 
       } else { // donation line
+        if (!currentDonorId) continue;
+        
         let rowObj = {donorId: currentDonorId};
 
         for (let cn = 1; cn <= 6; cn++) {
